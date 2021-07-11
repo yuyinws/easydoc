88faa32a6bdc187369735eb5a4ad988f3fc1a8f6
lyc <lyc@pheicloud.com>
2021-07-11 19:24:23 +0800

    t

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,4 +1,5 @@
 <template>
+    test page
     <div v-for="item in list" :key="item.sha">
       <div>{{item.name}}</div>
       <div>{{item}}</div>
@@ -6,23 +7,23 @@
 </template>
 
 <script setup>
-import service from '../utils/http'
-import config from '../config'
-import axios from 'axios'
-import {reactive} from 'vue'
-let list = reactive([1,2,3])
-const t = () => {
-    return service({
-        url: `https://api.github.com/repos/${config.userName}/${config.repo}/contents/doc?ref=gh-pages`,
-    })
-}
+// import service from '../utils/http'
+// import config from '../config'
+// import axios from 'axios'
+// import {reactive} from 'vue'
+// let list = reactive([1,2,3])
+// const t = () => {
+//     return service({
+//         url: `https://api.github.com/repos/${config.userName}/${config.repo}/contents/doc?ref=gh-pages`,
+//     })
+// }
 
-axios({
-  method:"GET",
-  url:`https://api.github.com/repos/${config.userName}/${config.repo}/contents/doc?ref=gh-pages&client_id=${config.clientId}&client_secret=${config.secrets}`
-}).then(res => {
-  console.log(res)
-})
+// axios({
+//   method:"GET",
+//   url:`https://api.github.com/repos/${config.userName}/${config.repo}/contents/doc?ref=gh-pages&client_id=${config.clientId}&client_secret=${config.secrets}`
+// }).then(res => {
+//   console.log(res)
+// })
 // t().then(res => {
 //   list.value = res.data
   
