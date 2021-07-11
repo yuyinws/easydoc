11a225db4154bb28f980f5ff072b11ba28d1fe0c
lyc <lyc@pheicloud.com>
2021-07-11 19:56:59 +0800

    ss

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,33 +1,37 @@
 <template>
-    test page
-    <div v-for="item in list" :key="item.sha">
-      <div>{{item.name}}</div>
-      <div>{{item}}</div>
+    <div v-for="item in _data"
+         :key="item.sha">
+        <div>{{item.name}}</div>
+        <div>{{item}}</div>
     </div>
 </template>
 
-<script setup>
-// import service from '../utils/http'
-// import config from '../config'
-// import axios from 'axios'
-// import {reactive} from 'vue'
-// let list = reactive([1,2,3])
-// const t = () => {
-//     return service({
-//         url: `https://api.github.com/repos/${config.userName}/${config.repo}/contents/doc?ref=gh-pages`,
-//     })
-// }
 
-// axios({
-//   method:"GET",
-//   url:`https://api.github.com/repos/${config.userName}/${config.repo}/contents/doc?ref=gh-pages&client_id=${config.clientId}&client_secret=${config.secrets}`
-// }).then(res => {
-//   console.log(res)
-// })
-// t().then(res => {
-//   list.value = res.data
-  
-// console.log(list)
-// })
 
+<script>
+import { reactive, ref,onMounted,toRefs } from 'vue'
+import axios from 'axios'
+import config from '../config'
+export default {
+    setup() {
+        const list = reactive({
+          _data:[]
+        })
+
+        const getData = () => {
+            axios({
+                method: 'GET',
+                url: `https://api.github.com/repos/${config.userName}/${config.repo}/contents/doc?ref=gh-pages&client_id=${config.clientId}&client_secret=${config.secrets}`,
+            }).then((res) => {
+                console.log(res)
+                list._data = res.data
+            })
+        }
+
+        onMounted(() => {
+          getData()
+        })
+        return { ...toRefs(list) }
+    },
+}
 </script>
