a1ce71c41182d84f98c483ab65934e04ace47de3
lyc <lyc@pheicloud.com>
2021-07-11 19:19:28 +0800

    t

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 export default {
   apiDomain: 'yuyinws.github.io',
+  userName:'yuyinws',
   repo: 'easydoc',
+  clientId: 'b46f59e6be5dff5ce391',
+  secrets:'6c555701c1d52183e9a8b4351365bb50914bcf6b'
 }
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,15 +1,32 @@
 <template>
-    <div>{{test}}</div>
+    <div v-for="item in list" :key="item.sha">
+      <div>{{item.name}}</div>
+      <div>{{item}}</div>
+    </div>
 </template>
 
 <script setup>
 import service from '../utils/http'
+import config from '../config'
+import axios from 'axios'
+import {reactive} from 'vue'
+let list = reactive([1,2,3])
 const t = () => {
     return service({
-        url: 'https://api.github.com/repos/yuyinws/easydoc/contents',
+        url: `https://api.github.com/repos/${config.userName}/${config.repo}/contents/doc?ref=gh-pages`,
     })
 }
-t().then((res) => {
-    console.log(res)
+
+axios({
+  method:"GET",
+  url:`https://api.github.com/repos/${config.userName}/${config.repo}/contents/doc?ref=gh-pages&client_id=${config.clientId}&client_secret=${config.secrets}`
+}).then(res => {
+  console.log(res)
 })
+// t().then(res => {
+//   list.value = res.data
+  
+// console.log(list)
+// })
+
 </script>
