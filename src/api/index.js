0be62eeec4e082b00f2f9604b5ff8d7997459242
lyc <lyc@pheicloud.com>
2021-10-05 11:29:12 +0800

    test

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import _axios from "../utils/http";
 import config from '../config'
-export function getCatalog () {
-  return _axios('get',`https://api.github.com/repos/${config.userName}/${config.repo}/contents/doc?ref=gh-pages&client_id=${config.clientId}&client_secret=${config.secrets}`)
+export function getCatalog (path = 'doc') {
+  return _axios('get',`https://api.github.com/repos/${config.userName}/${config.repo}/contents/${path}?ref=gh-pages&client_id=${config.clientId}&client_secret=${config.secrets}`)
 }
 
 export function getContent (path) {
--- src/views/fileList.vue
+++ src/views/fileList.vue
@@ -1,31 +1,59 @@
 <template>
   <van-loading class="loading" color="#1989fa" v-if="isLoading"></van-loading>
   <div v-else>
-    <div v-for="item in data.catalog" :key="item.sha">
+    <!-- <div v-for="item in data.catalog" :key="item.sha">
       <div @click="emits('getText', item)">{{ item.name }}</div>
-    </div>
+    </div>-->
+    <n-tree
+      block-line
+      :data="data.catalog"
+      selectable
+      key-field="sha"
+      label-field="name"
+      :on-update:selected-keys="test"
+    ></n-tree>
   </div>
 </template>
 
 <script setup>
 import { reactive, onMounted, ref } from 'vue'
 import { getCatalog } from '@/api/index.js'
+import { NTree } from 'wowjoy-vui'
 
 const emits = defineEmits(['getText'])
 const data = reactive({
   catalog: []
 })
-const isLoading = ref(false)
+const isLoading = ref(true)
 
-const _getCatalog = async () => {
-  isLoading.value = true
-  const response = await getCatalog()
-  data.catalog = response
-  isLoading.value = false
+const renderList = async (path,index = null) => {
+  const list = await _getCatalog(path)
+  let loadingFlag = false
+  if(index){
+    data.catalog[index].children = list
+  } else {
+    data.catalog = list
+  }
+  list.forEach((item,index) => {
+    if(item.type === 'dir') {
+      loadingFlag = true
+      renderList(item.path,index)
+    }
+  })
+  isLoading.value = loadingFlag
+}
+
+const _getCatalog = async (path) => {
+  const response = await getCatalog(path)
+  return response
+}
+
+const test = (event) => {
+  console.log(event)
 }
 
 onMounted(() => {
-  _getCatalog()
+  renderList()
 })
 </script>
 <style scoped>
