babe50d9085e39fcf74841800ecbb8dd13536a89
yuyinws <lyc657508366@gmail.com>
2021-12-20 14:39:18 +0800

    ci

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
-import * as core from '@actions/core';
-const repo = core.getInput('REPO')
 export default {
   apiDomain: 'git.yuyinws.top',
   userName: 'yuyinws',
-  repo:repo,
+  repo: easydoc,
   clientId: 'b46f59e6be5dff5ce391',
-  secrets:'6c555701c1d52183e9a8b4351365bb50914bcf6b'
+  secrets: '6c555701c1d52183e9a8b4351365bb50914bcf6b',
 }
