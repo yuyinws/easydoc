30d2a60d5490514598999b92d75d2e6167a30a31
yuyinws <lyc657508366@gmail.com>
2021-12-20 13:07:16 +0800

    CI

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 export default {
   apiDomain: 'git.yuyinws.top',
   userName:'yuyinws',
-  repo: 'easydoc',
   clientId: 'b46f59e6be5dff5ce391',
   secrets:'6c555701c1d52183e9a8b4351365bb50914bcf6b'
 }
